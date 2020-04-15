d7df01411032159f9962807d09a56578461bf222
Abdullah Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-04-15 19:04:20 +0200

    Edit Files

--- Task 2 - Elevator System/Codes/main.c
+++ Task 2 - Elevator System/Codes/main.c
@@ -3,11 +3,51 @@
 #include "lib/DIO.h"
 #include "lib/LED.h"
 #include "lib/button.h"
+#include "lib/interrupt.h"
 #include <stdio.h>
 
+void delay_ms(int millis);
+
 void main(void)
- { 
-   // Write your code here
-   while (1)
-      ;
- }
+{ 
+	interrupt_vInit();
+	interrupt_vEnableExternal(0);
+	interrupt_vEnableExternal(1);
+	
+	// Initialize LED On P1.1 and P2.0
+	LED_vInit('1', 0);
+	
+	// Initialize Button On P3.4
+	button_vInit('3', 2);
+	button_vInit('3', 3);
+	
+	while(1)
+	{	
+		LED_vTurnOff('1', 0);
+		delay_ms(50);
+	}
+}
+
+void EINT_BTN() interrupt 0
+{
+	LED_vTurnOn('1', 0);
+	delay_ms(1000);
+}
+
+void EINT_BTN2() interrupt 2
+{
+	LED_vTurnOn('1', 0);
+	delay_ms(3000);
+}
+
+
+void delay_ms(int millis)
+{
+	unsigned int i, j;
+	for(i = 0; i<millis; i++)
+	{
+		// Wait 1ms
+		for(j = 0; j<113; j++);
+	}
+	
+}
